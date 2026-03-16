fb7cb50052b36196ca19911d26f5b6e2646ccfcb
Your Name <you@example.com>
2026-03-16 21:19:06 +0300

    5 cdelal

---
+++ 0603.py
@@ -0,0 +1,28 @@
+'''for n in range(0,100):
+    a=bin(n)[2:]
+    if a.count("1") % 2 ==0:
+        a+="10"
+    else:
+        a+="00"
+    R=int(a,2)
+    print(R,n)'''
+'''for n in range(0,100):
+    a=bin(n)[2:]
+    a+=a[-1]
+    if a.count('1')%2==0:
+        a+='10'
+    else:
+        a+='00'
+    r=int(a,2)
+    print(r)'''
+'''for n in range(0,100):
+    a=bin(n)[2:]
+    if n%2==0:
+        a+='01'
+    else:
+        a+='10'
+    r=int(a,2)
+    print(r,n)'''
+    
+
+    
